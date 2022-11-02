d8db80d648a211f838af32eafc9cc8fe348918d0
dskrishnu <dskrishnu@hotmail.com>
2022-11-02 19:15:43 +0530

    Unit Test

---
+++ HelloWorldApp.Tests/MathOperationsTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using HelloWorldApp;
+using HelloWorldApp.Web;
+
+namespace HelloWorldApp.Web.Test
+{
+    public class MathOperationsTest
+        {
+            [Test()]
+            public void AddTest()
+            {
+                MathOperations bm = new MathOperations();
+                double res = bm.Add(10, 10);
+                Assert.AreEqual(res, 20);
+            }
+            [Test()]
+            public void SubtractTest()
+            {
+                MathOperations bm = new MathOperations();
+                double res = bm.Subtract(10, 10);
+                Assert.AreEqual(res, 0);
+            }
+            [Test()]
+            public void DivideTest()
+            {
+                MathOperations bm = new MathOperations();
+                double res = bm.Divide(10, 5);
+                Assert.AreEqual(res, 2);
+            }
+            [Test()]
+            public void MultiplyTest()
+            {
+                MathOperations bm = new MathOperations();
+                double res = bm.Multiply(10, 5);
+                Assert.AreEqual(res, 50);
+            }
+        }
+}

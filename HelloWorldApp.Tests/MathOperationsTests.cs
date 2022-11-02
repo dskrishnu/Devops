c2692c3c56957874a678c55f35acaaec37cec632
dskrishnu <dskrishnu@hotmail.com>
2022-11-02 18:28:56 +0530

    Unit Test

--- HelloWorldApp.Tests/MathOperationsTests.cs
+++ HelloWorldApp.Tests/MathOperationsTests.cs
@@ -7,37 +7,37 @@ using NUnit.Framework;
 using HelloWorldApp;
 using HelloWorldApp.Web;
 
-//namespace HelloWorldApp.Tests
-//{
-public class MathOperationsTests
-    {
-        [Test()]
-        public void AddTest()
+namespace HelloWorldApp.Tests
+{
+    public class MathOperationsTests
         {
-            MathOperations bm = new MathOperations();
-            double res = bm.Add(10, 10);
-            Assert.AreEqual(res, 20);
+            [Test()]
+            public void AddTest()
+            {
+                MathOperations bm = new MathOperations();
+                double res = bm.Add(10, 10);
+                Assert.AreEqual(res, 20);
+            }
+            [Test()]
+            public void SubtractTest()
+            {
+                MathOperations bm = new MathOperations();
+                double res = bm.Subtract(10, 10);
+                Assert.AreEqual(res, 0);
+            }
+            [Test()]
+            public void DivideTest()
+            {
+                MathOperations bm = new MathOperations();
+                double res = bm.Divide(10, 5);
+                Assert.AreEqual(res, 2);
+            }
+            [Test()]
+            public void MultiplyTest()
+            {
+                MathOperations bm = new MathOperations();
+                double res = bm.Multiply(10, 5);
+                Assert.AreEqual(res, 50);
+            }
         }
-        [Test()]
-        public void SubtractTest()
-        {
-            MathOperations bm = new MathOperations();
-            double res = bm.Subtract(10, 10);
-            Assert.AreEqual(res, 0);
-        }
-        [Test()]
-        public void DivideTest()
-        {
-            MathOperations bm = new MathOperations();
-            double res = bm.Divide(10, 5);
-            Assert.AreEqual(res, 2);
-        }
-        [Test()]
-        public void MultiplyTest()
-        {
-            MathOperations bm = new MathOperations();
-            double res = bm.Multiply(10, 5);
-            Assert.AreEqual(res, 50);
-        }
-    }
-//}
+}

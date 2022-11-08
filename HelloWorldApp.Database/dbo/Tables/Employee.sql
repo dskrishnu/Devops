6a622033c2be9ecb0ea782a7014887255bd99b4f
dskrishnu <dskrishnu@hotmail.com>
2022-11-08 11:27:19 +0530

    Database Project

---
+++ HelloWorldApp.Database/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]     INT           NOT NULL,
+    [Name]   NVARCHAR (50) NULL,
+    [Salary] MONEY         NULL,
+    [Experience] NCHAR(10) NULL, 
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ HelloWorldApp.Database/dbo/Tables/Finance.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Finance]
+(
+	[Id1] INT NOT NULL PRIMARY KEY, 
+    [Month] NVARCHAR(50) NULL, 
+    [Expenses] MONEY NULL
+)
